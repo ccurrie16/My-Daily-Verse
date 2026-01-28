fa20843c766d5a95409dbadb640842dd1bc6e852
ccurrie16 <codycurrie777@gmail.com>
2026-01-28 21:53:35 +0000

    Updated code to use new JSON

--- lib/models/verse.dart
+++ lib/models/verse.dart
@@ -1,18 +1,21 @@
 class Verse {
-  final String text;
   final String reference;
+  final String text;
 
   const Verse({
-    required this.text,
     required this.reference,
+    required this.text,
   });
-  Map<String, dynamic> toJson() => {
-        'text': text,
-        'reference': reference,
-      };
 
-  factory Verse.fromJson(Map<String, dynamic> json) => Verse(
-        text: (json['text'] ?? '').toString(),
-        reference: (json['reference'] ?? '').toString(),
-      );
+  factory Verse.fromJson(Map<String, dynamic> json) {
+    return Verse(
+      reference: (json['ref'] ?? '') as String,
+      text: (json['text'] ?? '') as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'ref': reference,
+        'text': text,
+      };
 }
