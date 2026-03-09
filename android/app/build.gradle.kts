8d6562e3fb4a6ce1bff690238801e68af637e955
Cody Currie <codycurrie777@gmail.com>
2026-03-09 11:08:42 -0400

    Set up iOS build and fix Google Sign-In

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,9 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
